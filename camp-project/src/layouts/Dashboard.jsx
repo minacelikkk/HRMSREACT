1064fc060a2814bce4ebb4dce9036756ef4a376b
minaveyisoglu <minaveyis1998@gmail.com>
2021-07-02 00:08:33 +0300

    Update Dashboard.jsx
    code refactored

--- camp-project/src/layouts/Dashboard.jsx
+++ camp-project/src/layouts/Dashboard.jsx
@@ -3,19 +3,20 @@ import JobTitleList from "../pages/JobTitle/JobTitleList";
 import JobAdvertisementList from "../pages/JobAdvertisement/JobAdvertisementList";
 import { Route } from 'react-router-dom'
 import JobAdvertisementAdd from "../pages/JobAdvertisement/JobAdvertisementAdd";
-
+import { ToastContainer } from "react-toastify";
 import { Grid} from "semantic-ui-react";
 import EmployerList from "../pages/Employer/EmployerList";
 
 export default function Dashboard() {
   return (
     <div>
+     <ToastContainer position="bottom-right"/>
       <Grid>
         <Grid.Row>
-          <Grid.Column width={5}>
-            <JobTitleList />
+          <Grid.Column width={4}>
+            <Categories />
           </Grid.Column>
-          <Grid.Column width={11}>
+          <Grid.Column width={12}>
             <Route exact path="/" component={JobAdvertisementList}/>
             <Route exact path="/jobTitle" component={JobTitleList}/>
             <Route exact path="/jobTitle/:title" component={JobTitleList}/>
